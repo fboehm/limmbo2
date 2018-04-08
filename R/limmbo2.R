2bd1749bbfd60020bc603c6d8b64d023b501b4d5
Frederick Boehm <frederick.boehm@gmail.com>
2018-04-08 16:19:47 -0500

    created limmbo2() function

---
+++ R/limmbo2.R
@@ -0,0 +1,19 @@
+#' Estimate Vg and Ve with limmbo
+#'
+#' @param kinship a n by n kinship matrix
+#' @param pheno a n by d phenotypes matrix
+#' @param niter integer number of iterations for limix calculations
+#' @param S number of phenotypes per bootstrap sample
+#' @param seed positive integer random seed
+#'
+limmbo2 <- function(kinship, pheno, niter = 10, S = 2, seed){
+    t(chol(kinship)) -> chol_kin
+    prep_data(pheno, chol_kin) -> input_data
+    make_limmbo(input_data, TRUE, niter, S) -> l_out
+    bs_covar_est(l_out, 1, seed) -> bs_out
+    bs_out2 <- lapply(FUN = convert_for_bscombine, X = bs_out)
+    combine_bs(bs_out2, l_out) -> fits
+    fits$Cn_fit -> Ve
+    fits$Cg_fit -> Vg
+    return(list(Vg = Vg, Ve = Ve))
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(limmbo2)
+
+test_check("limmbo2")
